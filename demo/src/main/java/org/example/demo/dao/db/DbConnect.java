39ce7da5ec60593acefaff029810f286c01b3414
Lê Anh Tuấn <22130310@st.hcmuaf.edu.vn>
2024-12-19 22:06:16 +0700

    kết nối csdl

---
+++ demo/src/main/java/org/example/demo/dao/db/DbConnect.java
@@ -0,0 +1,33 @@
+package org.example.demo.dao.db;
+
+import java.sql.*;
+public class DbConnect {
+    static String url = "jdbc:mysql://" + DbProperties.host() + ":" + DbProperties.port() + "/" + DbProperties.dbName() + "?" + DbProperties.option();
+    static Connection conn;
+    public static Statement get() {
+//        try {
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//            conn = DriverManager.getConnection(url, DbProperties.username(), DbProperties.password());
+//            return conn.createStatement();
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+        try {
+            if(conn==null || conn.isClosed()){
+                makeConnection();
+            }
+        return conn.createStatement();
+        } catch (SQLException | ClassNotFoundException e) {
+            return null;
+        }
+    }
+
+    private static void makeConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        conn=DriverManager.getConnection(url, DbProperties.username(), DbProperties.password());
+    }
+
+    public static void main(String[] args) {
+        DbConnect.get();
+    }
+}
