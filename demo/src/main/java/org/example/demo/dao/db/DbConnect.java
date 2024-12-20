796fc4d8e43dcd25c99166bae34057fc4bffc2ae
Lê Anh Tuấn <22130310@st.hcmuaf.edu.vn>
2024-12-20 13:16:25 +0700

    Update DbConnect.java

--- demo/src/main/java/org/example/demo/dao/db/DbConnect.java
+++ demo/src/main/java/org/example/demo/dao/db/DbConnect.java
@@ -1,9 +1,11 @@
 package org.example.demo.dao.db;
 
 import java.sql.*;
+
 public class DbConnect {
     static String url = "jdbc:mysql://" + DbProperties.host() + ":" + DbProperties.port() + "/" + DbProperties.dbName() + "?" + DbProperties.option();
     static Connection conn;
+
     public static Statement get() {
 //        try {
 //            Class.forName("com.mysql.cj.jdbc.Driver");
@@ -13,10 +15,10 @@ public class DbConnect {
 //            throw new RuntimeException(e);
 //        }
         try {
-            if(conn==null || conn.isClosed()){
+            if (conn == null || conn.isClosed()) {
                 makeConnection();
             }
-        return conn.createStatement();
+            return conn.createStatement();
         } catch (SQLException | ClassNotFoundException e) {
             return null;
         }
@@ -24,10 +26,14 @@ public class DbConnect {
 
     private static void makeConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        conn=DriverManager.getConnection(url, DbProperties.username(), DbProperties.password());
+        conn = DriverManager.getConnection(url, DbProperties.username(), DbProperties.password());
     }
 
     public static void main(String[] args) {
-        DbConnect.get();
+        if (DbConnect.get() != null) {
+            System.out.println("Connected");
+        } else {
+            System.out.println("Not Connected");
+        }
     }
 }
