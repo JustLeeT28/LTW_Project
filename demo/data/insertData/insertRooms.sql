d566524a8f4076a8789f348b69f4b6c1f76f0454
Lê Anh Tuấn <22130310@st.hcmuaf.edu.vn>
2024-12-27 21:18:34 +0700

    thêm 2 file query để insert data vào bảng rooms và seats

---
+++ demo/data/insertData/insertRooms.sql
@@ -0,0 +1,5 @@
+INSERT INTO rooms (name, style, occupancy, status) VALUES ('Room1', '2D', 50, 'active');
+INSERT INTO rooms (name, style, occupancy, status) VALUES ('Room2', '3D', 60, 'inactive');
+INSERT INTO rooms (name, style, occupancy, status) VALUES ('Room3', '4Dx', 70, 'active');
+INSERT INTO rooms (name, style, occupancy, status) VALUES ('Room4', 'Imax', 50, 'inactive');
+INSERT INTO rooms (name, style, occupancy, status) VALUES ('Room5', '2D', 60, 'active');
