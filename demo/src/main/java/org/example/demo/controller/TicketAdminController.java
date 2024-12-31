858fd03c67880eb49b1dae3513b87b2629f1532a
Phạm Minh Hiếu <21130055@st.hcmuaf.edu.vn>
2024-12-31 16:52:06 +0700

    add controller + debug

---
+++ demo/src/main/java/org/example/demo/controller/TicketAdminController.java
@@ -0,0 +1,38 @@
+package org.example.demo.controller;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import org.example.demo.dao.models.MovieTicket;
+import org.example.demo.service.TicketAdminService;
+
+import java.io.IOException;
+import java.util.List;
+
+public class TicketAdminController extends HttpServlet {
+    private TicketAdminService ticketService;
+
+    @Override
+    public void init() {
+        ticketService = (TicketAdminService) getServletContext().getAttribute("ticketService");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        try {
+            if ("search".equals(action)) {
+                String keyword = request.getParameter("keyword");
+                List<MovieTicket> tickets = ticketService.searchTickets(keyword);
+                request.setAttribute("tickets", tickets);
+            } else {
+                List<MovieTicket> tickets = ticketService.getAllBookedTickets();
+                request.setAttribute("tickets", tickets);
+            }
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/admin/tikket_mng.jsp");
+            dispatcher.forward(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Error while managing tickets.");
+        }
+    }
+}
