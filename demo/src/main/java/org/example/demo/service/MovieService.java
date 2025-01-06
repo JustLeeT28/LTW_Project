a2515292708dc9c92e28105760b4c4430ad1edd2
Lê Anh Tuấn <22130310@st.hcmuaf.edu.vn>
2025-01-06 22:45:26 +0700

    Update MovieService.java
    thêm phương thức lấy đạo diễn và diễn viên

--- demo/src/main/java/org/example/demo/service/MovieService.java
+++ demo/src/main/java/org/example/demo/service/MovieService.java
@@ -1,12 +1,18 @@
 package org.example.demo.service;
 
+import org.example.demo.dao.ActorDao;
+import org.example.demo.dao.DirectorDao;
 import org.example.demo.dao.MovieDao;
+import org.example.demo.dao.model.Actor;
+import org.example.demo.dao.model.Director;
 import org.example.demo.dao.model.Movie;
 
 import java.util.List;
 
 public class MovieService {
     MovieDao movieDao = new MovieDao();
+    DirectorDao directorDao = new DirectorDao();
+    ActorDao actorDao = new ActorDao();
 
     public List<Movie> getMovies() {
         return movieDao.getMovies();
@@ -18,5 +24,11 @@ public class MovieService {
 
     public Movie getMovieById(int id) {
         return movieDao.getMovieById(id);
+    }
+    public List<Director> getDirectors(int id) {
+        return directorDao.getMovieDirectors(id);
+    }
+    public List<Actor> getActors(int id) {
+        return actorDao.getMovieActors(id);
     }
 }
