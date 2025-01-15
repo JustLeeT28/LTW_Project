e2f8c236e8a210ab9b3863a9726417d7c572a0a4
Lê Anh Tuấn <22130310@st.hcmuaf.edu.vn>
2025-01-16 00:42:09 +0700

    Update SeatDao.java
    thêm phương thức lấy ghế trong phòng, và lấy ghế theo showtimeId và phòng

--- demo/src/main/java/org/example/demo/dao/SeatDao.java
+++ demo/src/main/java/org/example/demo/dao/SeatDao.java
@@ -1,15 +1,16 @@
 package org.example.demo.dao;
 
-import com.mysql.cj.jdbc.JdbcConnection;
 import org.example.demo.dao.db.DbConnect;
-import org.example.demo.dao.model.Movie;
 import org.example.demo.dao.model.Seat;
+import org.example.demo.dao.model.ShowSeat;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class SeatDao {
     public List<Seat> getSeatsInRoom(int roomId) {
@@ -28,9 +29,9 @@ public class SeatDao {
                         resultSet.getInt("id"),
                         resultSet.getInt("roomId"),
                         resultSet.getString("row"),
-                        resultSet.getString("seatNumber"),
+                        resultSet.getString("seat_number"),
                         resultSet.getDouble("price"),
-                        resultSet.getString("status")
+                        resultSet.getString("isActive")
                 );
                 seats.add(movie);
             }
@@ -48,6 +49,61 @@ public class SeatDao {
             } catch (SQLException e) {
                 e.printStackTrace(); // Log lỗi khi đóng tài nguyên
             }
+        }
+    }
+
+    public List<ShowSeat> getStatusSeatByShowtimeId(int showtimeId, int roomId) {
+        PreparedStatement ps = null;
+        ResultSet resultSet = null;
+        try {
+            String query = "SELECT * FROM showtime_seats WHERE showtimeId = ? AND roomId = ?";
+
+            ps = DbConnect.get(query); // Lấy PreparedStatement từ DbConnect
+            ps.setInt(1, showtimeId); // Set tham số cho PreparedStatement
+            ps.setInt(2, roomId);
+            resultSet = ps.executeQuery(); // Thực thi truy vấn
+            List<ShowSeat> seats = new ArrayList<>();
+            while (resultSet.next()) {
+                ShowSeat movie = new ShowSeat(
+                        resultSet.getInt("id"),
+                        resultSet.getInt("showtimeId"),
+                        resultSet.getInt("seatId"),
+                        resultSet.getString("status"),
+                        resultSet.getInt("roomId")
+                );
+                seats.add(movie);
+            }
+
+            return seats;
+
+        } catch (SQLException e) {
+            e.printStackTrace(); // Log lỗi
+            return new ArrayList<>(); // Trả về danh sách rỗng khi gặp lỗi
+        } finally {
+            // Đảm bảo đóng tài nguyên sau khi sử dụng
+            try {
+                if (resultSet != null) resultSet.close();
+                if (ps != null) ps.close();
+            } catch (SQLException e) {
+                e.printStackTrace(); // Log lỗi khi đóng tài nguyên
+            }
+        }
+    }
+
+    public Set<String> getRowInRoom(int roomId) {
+        List<Seat> seats = getSeatsInRoom(roomId);
+        Set<String> rows = new HashSet<>();
+        for (Seat seat : seats) {
+            rows.add(seat.getRow());
+        }
+        return rows;
+    }
+
+    public static void main(String[] args) {
+        SeatDao seatDao = new SeatDao();
+        List<Seat> seats = seatDao.getSeatsInRoom(1);
+        for (Seat seat : seats) {
+            System.out.println(seat);
         }
     }
 }
