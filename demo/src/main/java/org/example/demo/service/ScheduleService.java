ccab6f91d9e53180e40e07dd4469ba86e0741b59
Nguyễn Duy Thịnh <22130267@st.hcmuaf.edu.vn>
2025-01-16 00:36:58 +0700

    create scheduleService

---
+++ demo/src/main/java/org/example/demo/service/ScheduleService.java
@@ -0,0 +1,36 @@
+package org.example.demo.service;
+
+import org.example.demo.dao.ScheduleDao;
+import org.example.demo.dao.model.Seat;
+import org.example.demo.dao.model.Showtime;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+public class ScheduleService {
+    ScheduleDao scheduleDao = new ScheduleDao();
+    public boolean isConflict(String movieId,String roomId, String showDate, String showTime) {
+        return scheduleDao.isConflict(movieId,roomId,showDate,showTime);
+    }
+
+    public void addShowTime(String movieId, String roomId, String showDate, String showTime) {
+        scheduleDao.addShowTime(movieId,roomId,showDate,showTime);
+    }
+
+    public Showtime GetShowtime(int movieIdInt, int roomIdInt, LocalDate localShowDate, LocalTime localShowTime) {
+        return scheduleDao.GetShowTime(movieIdInt,roomIdInt,localShowDate,localShowTime);
+    }
+
+//    public void addShowAndSeat(int id, String movieId) {
+//        scheduleDao.addShowAndSeats(id,movieId);
+//    }
+
+    public List<Seat> getListSeatsByRoomid(String roomId) {
+        return scheduleDao.GetSeatByRoom(roomId);
+    }
+
+    public void addSh_S(int id, int id1, String isActive) {
+        scheduleDao.addShow_seats(id,id1,isActive);
+    }
+}
